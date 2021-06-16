14ad7e80e76a1631d34d769d2eaa66274278dc00
thenerdie <stirmanjr@gmail.coom>
2021-06-16 16:58:17 -0400

    Handle failure, use string replacement to be able to change the format quickly

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const axios = require('axios')
 const config = require("./config")
 
 setInterval(async () => {
-    let response = await axios.get(`${config.url}/${config.station}`)
-    let data = response.data
+    try {
+        let response = await axios.get(`${config.url}/${config.station}`)
+        let data = response.data
 
-    const out = `${data.now_playing.song.title} by ${data.now_playing.song.artist} on ${data.station.name}`
+        const out = config.text
+            .replace(/\[artist\]/g, data.now_playing.song.artist)
+            .replace(/\[title\]/g, data.now_playing.song.title)
+            .replace(/\[station\]/g, data.station.name)
 
-    fs.writeFile(config.file, out, () => {})
+        fs.writeFile(config.file, out, () => {})
+    } catch {
+        fs.writeFile(config.file, config.default, () => {})
+    }
 }, 10000)
