77a878c4db39b44b2bef2d82d33cec9fd402b2da
thenerdie <stirmanjr@gmail.coom>
2021-06-17 18:46:57 -0400

    Implement handlebars

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const axios = require('axios')
+const handlebars = require('handlebars')
 
 const config = require("./config")
+
+const templateText = fs.readFileSync("./template.txt", "utf8")
+const template = handlebars.compile(templateText)
 
 setInterval(async () => {
     try {
         let response = await axios.get(`${config.url}/${config.station}`)
         let data = response.data
 
-        const out = config.text
-            .replace(/\[artist\]/g, data.now_playing.song.artist)
-            .replace(/\[title\]/g, data.now_playing.song.title)
-            .replace(/\[station\]/g, data.station.name)
+        const out = template(data)
 
         fs.writeFile(config.file, out, () => {})
     } catch {
